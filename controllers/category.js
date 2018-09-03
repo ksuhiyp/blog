931e679a4397c96d697e212289f02e7485c001e5
Idea <ksuhiyp@gmail.com>
2018-09-03 15:31:40 +0300

    Categories API inhancment

--- controllers/category.js
+++ controllers/category.js
@@ -9,67 +9,62 @@ exports.getCategoryById = (req, res, next) => {
     req.query.options ? options = req.query.options : options = null
     categoriesModel.findById(req.params._id, options).
         select('-__v ').
-        exec((err, category) => {
-            if (err)
-                return next(err);
+        exec().
+        then((category) => {
             if (!category)
-                return next(createError(204));
+                throw new Error("Category Not Found!")
             res.status(200).json({ "operation": "GET/Category", "Category": category })
-
-        })
+        }).catch(err => next(err))
 }
 
 exports.createCategory = (req, res, next) => {
     category = new categoriesModel(req.body);
-    category.save((err, category) => {
-        if (err)
-            return next(err);
-        if (!category)
-            return next(createError(204));
+    category.save().then((category) => {
+
         res.status(200).json({ "operation": "POST/Category", "Category": category })
 
-    });
+    }).catch(err => next(err))
 
 }
 /**var doc = parent.children.id(_id); */
 exports.getAllCategories = (req, res, next) => {
     categoriesModel.find().
-        select('-__v -articles').
-        exec((err, categories) => {
-            if (err)
-                return next(err);
+        select('-__v').
+        exec().
+        then((categories) => {
             if (!categories)
-                return next(createError(204, 'No Categories Found!'));
-            res.status(200).json({ "operation": "Get All Categories", "Categories": categories })
-        })
+                throw new Error('No Categories Found!');
+            res.status(200).json({ "operation": "Get/allCategories", "Count": categories.length, "Categories": categories })
+        }).catch(err => next(err))
 }
 exports.deleteCategory = (req, res, next) => {
-    if (!req.params._id)
-        return next(createError(400, 'missing Parameters!'));
 
     if (!errorHandler.validateObjId(req.params._id))
         return next(createError(400, "Invalid ID!"));
 
-    categoriesModel.findByIdAndRemove(req.params._id, (err, category) => {
-        if (err)
-            return next(err);
-        if (!category)
-            return next(createError(400, "category not found!"));
+    categoriesModel.findByIdAndRemove(req.params._id).
+        then((category) => {
+            if (!category)
+                throw new Error('category not found!');
 
-        res.status(201).json({ "operation": "DELETE/Category", "result": category._id })
+            res.status(201).json({ "operation": "DELETE/Category", "result": category._id })
 
-    })
+        }).catch(err => next(err))
 }
 /**we need a way to assert that req.body is not empty */
 exports.updateCategory = (req, res, next) => {
-    categoriesModel.findByIdAndUpdate(req.params._id, req.body, { new: true, runValidators: true }, (err, category) => {
-        if (err)
-            return next(createError(400, err));
-        if (!category)
-            return next(createError(400, "Category not found"));
-        res.status(200).json({ "operation": "PUT/Category", "updateCategory": category });
+    if (!errorHandler.validateObjId(req.params._id))
+        return next(createError(400, "Invalid ID!"));
 
+    categoriesModel.
+        findById(req.params._id).
+        then((category) => {
+            if (!category)
+                throw new Error('Tag Not Found!');
 
-    });
-
+            category.title = req.body.title;
+            category.save().then((category) => {
+                res.status(200).json({ "operation": "PUT/category", "updateCategory": category })
+            }).catch((err) => next(err));
+        }).catch((err) => next(createError(500, err)));
 }
--- routes/category.js
+++ routes/category.js
@@ -2,10 +2,10 @@ const app = require('express');
 const router = app.Router();
 const categoryController = require('../controllers/category');
 
-router.post('/', categoryController.createCategory);
-router.get('/', categoryController.getAllCategories);
-router.get('/:_id', categoryController.getCategoryById);
-router.delete('/:_id', categoryController.deleteCategory);
-router.put('/:_id', categoryController.updateCategory);
+router.post('/', categoryController.createCategory)
+    .get('/', categoryController.getAllCategories)
+    .get('/:_id', categoryController.getCategoryById)
+    .delete('/:_id', categoryController.deleteCategory)
+    .put('/:_id', categoryController.updateCategory);
 
 module.exports = router;
